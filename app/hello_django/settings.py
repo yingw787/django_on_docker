2212ccbe544f4b9a7d5aaac3b48d21371dc75069
Ying Wang <yingw787.personal@gmail.com>
2020-03-11 11:44:02 -0400

    Update settings.py update SECRET_KEY DEBUG and ALLOWED_HOSTS to be fetched from the environment instead of hardcoded.

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5=#r^*-=3b^t#-yvopod4uf7!b2sf)vb3ji&pt)m#jk=oy)vw+'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between
+# each. For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
